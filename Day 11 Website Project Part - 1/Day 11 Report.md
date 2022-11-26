69ca84958d4009891a168ec6c3f55d2f9b337aec
Yash <contactyashgulati@gmail.com>
2022-11-26 19:50:02 +0530

    Day 11 Website Project Part - 1

---
+++ Day 11 Website Project Part - 1/Day 11 Report.md
@@ -0,0 +1,22 @@
+# Day 11 JavaScript Report
+
+## 26 November 2022
+
+Hello this is my **Eleventh Report** for **JavaScript**.
+
+So, Today I Learned some topics from Namaste JavaScript Course are below:
+
+- None
+
+And from The Creative JavaScript Course are below:
+
+- Added Some Feautures
+  - Basic HTML
+  - Basic Styling
+  - Scroll Animation
+  - Cursor Animation
+- Learned About New Library (Scroll Magic)
+
+I Finished 8 Videos of Dev Ed's The Creative JavaScript Course and 0 Videos of Akshay Saini's Namaste JavaScript Course.
+
+Well that were's my learning for today. Bye Bye :wave:
---
+++ Day 11 Website Project Part - 1/Project/app.js
@@ -0,0 +1,81 @@
+let controller;
+let slideScene;
+let pageScene;
+
+function animationSlides() {
+  // Init Controller
+  controller = new ScrollMagic.Controller();
+  // Select Some Things
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
+  // Loop over each slide
+  sliders.forEach((slide, index, slides) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+    // GSAP
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+    // Create Scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addIndicators({ name: "slide" })
+      .addTo(controller);
+    // New Animation
+    const pageTl = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+    // Create New Scene
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .addIndicators({ name: "page", indent: 200 })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
+      .addTo(controller);
+  });
+}
+
+let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector("span");
+
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+
+function activecursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    mouseTxt.innerText = "Tap";
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseTxt.innerText = "";
+    gsap.to(".title-swipe", 1, { y: "100%" });
+  }
+}
+
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activecursor);
+
+animationSlides();
